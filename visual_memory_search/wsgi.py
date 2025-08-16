5d4ef5b2273aa56bd6c7f83c22c58d0760848f50
Jack <xjlin0@gmail.com>
2025-08-16 14:09:36 -0700

    Fix Vercel deployment: expose Django app as 'app' variable for serverless functions

--- visual_memory_search/wsgi.py
+++ visual_memory_search/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'visual_memory_search.settings')
 
 application = get_wsgi_application()
+# Vercel requires the WSGI application to be named 'app'
+app = application
